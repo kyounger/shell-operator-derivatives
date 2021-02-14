1b5efaf7d28045bce7dc49ad6418150e12045ac9
Kenny Younger <kyounger@gmail.com>
2021-02-14 12:31:56 -0600

    Use build arg for tag to make running this more often simpler

--- shell-operator-with-curl/build.sh
+++ shell-operator-with-curl/build.sh
@@ -1,2 +1,4 @@
-docker build . -t kyounger/shell-operator-with-curl
-#docker push kyounger/shell-operator-with-curl:latest
+export TAG="v1.0.0-rc.1"
+docker build . -t kyounger/shell-operator-with-curl:$TAG -t kyounger/shell-operator-with-curl:latest --build-arg TAG=$TAG
+docker push kyounger/shell-operator-with-curl:$TAG
+docker push kyounger/shell-operator-with-curl:latest
